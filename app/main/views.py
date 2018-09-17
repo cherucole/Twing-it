051c09ab670d1c1af3019c1b037ee833eb4a23e2
melisamalala <melissamalala@gmail.com>
2018-09-17 17:15:12 +0300

    Added Admin Dashboard

--- app/main/views.py
+++ app/main/views.py
@@ -71,3 +71,23 @@ def check_admin():
     if not current_user.is_admin:
         abort(403)
 
+
+
+
+@main.route('/dashboard')
+@login_required
+def dashboard():
+    """
+    Render the dashboard template on the /dashboard route
+    """
+    return render_template('dashboard.html', title="Dashboard")
+
+@main.route('/admin/dashboard')
+@login_required
+def admin_dashboard():
+    # prevent non-admins from accessing the page
+    if not current_user.is_admin:
+        abort(403)
+
+    return render_template('admin_dashboard.html', title="Dashboard")
+
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://melissamalala:abc123@localhost/twing'
+    SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://melissamalala:abc123@localhost/twing'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
