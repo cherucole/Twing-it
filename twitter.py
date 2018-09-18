25557b95deb6b88f4d9699673d30cef01a89a135
melisamalala <melissamalala@gmail.com>
2018-09-18 12:17:34 +0300

    Deleted api key

--- twitter.py
+++ twitter.py
@@ -61,51 +61,67 @@ query = "#ISupportUhuruCuts"
 # Language code (follows ISO 639-1 standards)
 language = "en"
 rpp=10
+
 geocode = "0.0236,37.9062,1000km"
 
 # Calling the user_timeline function with our parameters
 results = api.search(q=query, lang=language, geocode=geocode, rpp=rpp)
 
-# foreach through all tweets pulled
-# for tweet in results:
-#    # printing the text stored inside the tweet object
-#    # print (tweet.user.screen_name,"Tweeted:",tweet.text)
-#  # add to JSON
-#     with open('tweets.json', 'w', encoding='utf8') as file:
-#       json.dump(tweet._json, file)
-#
+
+# # secondly-retrieve_tweets_from_respective_keyword(s)
+# public_tweets = api.search('Ethiopia')
 
 for tweet in results:
+   # print(tweet.text)
+   print (tweet.user.screen_name,"Tweeted:",tweet.text)
 
-        # add to JSON
+   # finally-perform_sentiment_analysis_on_tweets
+   analysis = TextBlob(tweet.text)
+   print(analysis.sentiment)
+   print("")
 
-   with open('tweets.json', 'w', encoding='utf8') as file:
-    json.dump(tweet._json, file)
-   print(tweet)
-
-#
-# # Postgresql initialization
-# connection = psycopg2.connect("dbname=twing user=melissamalala")
-# cursor = connection.cursor()
-#
-# # The table schema: CREATE TABLE tweets (id SERIAL PRIMARY KEY, tweet_id BIGINT NOT NULL, text VARCHAR NOT NULL, screen_name VARCHAR NOT NULL, author_id INTEGER, created_at VARCHAR NOT NULL, inserted_at TIMESTAMP NOT NULL)
-#
-# try:
-#     statuses = api.list_timeline(api.me().screen_name, '<NAME OF TIMELINE?>')
-#     for s in statuses:
-#         # To remove duplicate entries
-#         # See http://initd.org/psycopg/docs/faq.html for "not all arguments converted during string formatting"
-#         cursor.execute("SELECT id FROM tweets WHERE text = %s;", [s.text])
-#         if cursor.rowcount == 0:
-#             cursor.execute("INSERT INTO tweets (tweet_id, text, screen_name, author_id, created_at, inserted_at) VALUES (%s, %s, %s, %s, %s, current_timestamp);", (s.id, s.text, s.author.screen_name, s.author.id, s.created_at))
-#             connection.commit()
-# except tweepy.error.TweepError:
-#     print "Whoops, could not fetch news!"
-# except UnicodeEncodeError:
-#     pass
-# finally:
-#     cursor.close()
-#     connection.close()
 #
 #
+# # foreach through all tweets pulled
+# # for tweet in results:
+# #    # printing the text stored inside the tweet object
+# #    # print (tweet.user.screen_name,"Tweeted:",tweet.text)
+# #  # add to JSON
+# #     with open('tweets.json', 'w', encoding='utf8') as file:
+# #       json.dump(tweet._json, file)
+# #
 #
+# for tweet in results:
+#
+#         # add to JSON
+#
+#    with open('tweets.json', 'w', encoding='utf8') as file:
+#     json.dump(tweet._json, file)
+#    print(tweet)
+#
+# #
+# # # Postgresql initialization
+# # connection = psycopg2.connect("dbname=twing user=melissamalala")
+# # cursor = connection.cursor()
+# #
+# # # The table schema: CREATE TABLE tweets (id SERIAL PRIMARY KEY, tweet_id BIGINT NOT NULL, text VARCHAR NOT NULL, screen_name VARCHAR NOT NULL, author_id INTEGER, created_at VARCHAR NOT NULL, inserted_at TIMESTAMP NOT NULL)
+# #
+# # try:
+# #     statuses = api.list_timeline(api.me().screen_name, '<NAME OF TIMELINE?>')
+# #     for s in statuses:
+# #         # To remove duplicate entries
+# #         # See http://initd.org/psycopg/docs/faq.html for "not all arguments converted during string formatting"
+# #         cursor.execute("SELECT id FROM tweets WHERE text = %s;", [s.text])
+# #         if cursor.rowcount == 0:
+# #             cursor.execute("INSERT INTO tweets (tweet_id, text, screen_name, author_id, created_at, inserted_at) VALUES (%s, %s, %s, %s, %s, current_timestamp);", (s.id, s.text, s.author.screen_name, s.author.id, s.created_at))
+# #             connection.commit()
+# # except tweepy.error.TweepError:
+# #     print "Whoops, could not fetch news!"
+# # except UnicodeEncodeError:
+# #     pass
+# # finally:
+# #     cursor.close()
+# #     connection.close()
+# #
+# #
+# #
