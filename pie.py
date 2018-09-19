2a45f77ce33025c471bc639a1a98b5ddd85c65f1
melisamalala <melissamalala@gmail.com>
2018-09-19 10:22:26 +0300

    Can redirect to PLOTLY

---
+++ pie.py
@@ -0,0 +1,9 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+labels = ['Oxygen','Hydrogen','Carbon_Dioxide','Nitrogen']
+values = [4500,2500,1053,500]
+
+trace = go.Pie(labels=labels, values=values)
+
+py.iplot([trace], filename='basic_pie_chart')
